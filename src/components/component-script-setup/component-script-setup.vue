eb66d08d91a933cccc03baca372d748fe61de000
Martin <mr.mmaurer+work@gmail.com>
2021-08-27 23:12:56 +0100

    WIP typed emits

--- src/components/component-script-setup/component-script-setup.vue
+++ src/components/component-script-setup/component-script-setup.vue
@@ -17,13 +17,31 @@ import {
 // https://github.com/vuejs/vue-next/issues/4294#issuecomment-896082769
 // import type { Props, Emits } from './component-script-setup.types';
 
+// https://github.com/vuejs/vue-next/issues/4408
+type Emits2<T> = {
+  [K in keyof T as `on${Capitalize<K & string>}`]?: T[K];
+};
+
+type KeyNames<T> = {
+  [K in keyof T]: `${K & string}`;
+}[keyof T];
+
+// Props
 export type Props = {
   title: string,
   initialCounterValue?: number
 }
 
+// Emits
+const eventNames = Object.freeze([
+  'update-counter',
+] as const);
+
+type EventNames = keyof typeof eventNames
+
+type EmitsEntry = (event: EventNames, payload: number | string) => void
+
 export type Emits = {
-  // (event: Event['type'], payload: number | string): void
   (event: 'update-counter', payload: number): void
 }
 
